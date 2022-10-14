16e4cba45c3b386c5886ed6c2cc253ae0dc48d79
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-10-14 15:54:52 -0500

    Update FailedToDetermineTypeDeclarationsException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDetermineTypeDeclarationsException.php
+++ src/Exception/FailedToDetermineTypeDeclarationsException.php
@@ -9,4 +9,18 @@ use RuntimeException;
 
 final class FailedToDetermineTypeDeclarationsException extends RuntimeException implements EventDispatcherExceptionInterface
 {
+    public static function invalidTypeDeclarations(string $name, string $type): self
+    {
+        return new self(sprintf('Invalid type declarations for "$%s" parameter; %s given.', $name, $type));
+    }
+
+    public static function missingFirstParameter(): self
+    {
+        return new self('Missing first parameter, "$event".');
+    }
+
+    public static function missingTypeDeclarations(string $name): self
+    {
+        return new self(sprintf('Missing type declarations for "$%s" parameter.', $name));
+    }
 }
