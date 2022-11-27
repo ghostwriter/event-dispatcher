2169d9ff1a0f43c90d39b23704a1c71b38453181
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 20:22:58 -0600

    Update DispatcherInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/DispatcherInterface.php
+++ src/Contract/DispatcherInterface.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Contract;
 
-use Psr\EventDispatcher\EventDispatcherInterface as PsrEventDispatcherInterface;
 use Throwable;
 
 /**
  * Delegates dispatching an event to one or more dispatchers.
  */
-interface DispatcherInterface extends PsrEventDispatcherInterface
+interface DispatcherInterface
 {
     /**
      * Provide all relevant listeners an event to process.
      *
-     * @template TEvent of object
-     *
-     * @param TEvent $event the object to process
-     *
      * @throws Throwable
-     *
-     * @return TEvent the Event that was passed, now modified by listeners
      */
-    public function dispatch(object $event): object;
+    public function dispatch(EventInterface $event): EventInterface;
 }
