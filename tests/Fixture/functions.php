2e2903a93c0cc0ef487cdbd5f127d9a4054a5b02
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-07 14:58:13 -0600

    Update functions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/functions.php
+++ tests/Fixture/functions.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Tests\Fixture;
 
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEvent;
-
 function listenerFunction(TestEvent $testEvent): void
 {
     $testEvent->write(__METHOD__);
