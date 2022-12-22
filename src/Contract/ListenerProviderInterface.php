0fa79656b603efe3e921cc6f82c2aadd1e5d388e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-21 22:51:32 -0600

    Update ListenerProviderInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ListenerProviderInterface.php
+++ src/Contract/ListenerProviderInterface.php
@@ -6,6 +6,7 @@ namespace Ghostwriter\EventDispatcher\Contract;
 
 use Generator;
 use Ghostwriter\Container\Contract\ContainerExceptionInterface;
+use Ghostwriter\EventDispatcher\Listener;
 
 /**
  * Maps registered Listeners, Providers and Subscribers.
@@ -13,8 +14,8 @@ use Ghostwriter\Container\Contract\ContainerExceptionInterface;
 interface ListenerProviderInterface
 {
     /**
-     * @param callable(EventInterface):void            $listener
-     * @param null|class-string<EventInterface>|string $event
+     * @param callable(EventInterface<bool>):void            $listener
+     * @param null|class-string<EventInterface<bool>>|string $event
      */
     public function addListener(
         callable $listener,
@@ -24,84 +25,17 @@ interface ListenerProviderInterface
     ): string;
 
     /**
-     * @param callable(EventInterface):void            $listener
-     * @param null|class-string<EventInterface>|string $event
-     */
-    public function addListenerAfter(
-        string $listenerId,
-        callable $listener,
-        ?string $event = null,
-        ?string $id = null
-    ): string;
-
-    /**
-     * @param callable(EventInterface):void            $listener
-     * @param null|class-string<EventInterface>|string $event
-     */
-    public function addListenerBefore(
-        string $listenerId,
-        callable $listener,
-        ?string $event = null,
-        ?string $id = null
-    ): string;
-
-    /**
-     * @param class-string<EventInterface>|string $event
-     */
-    public function addListenerService(
-        string $event,
-        string $listener,
-        int $priority = 0,
-        ?string $id = null
-    ): string;
-
-    /**
-     * @param class-string<EventInterface>|string $event
-     */
-    public function addListenerServiceAfter(
-        string $listenerId,
-        string $event,
-        string $listener,
-        int $priority = 0,
-        ?string $id = null
-    ): string;
-
-    /**
-     * @param class-string<EventInterface>|string $event
-     */
-    public function addListenerServiceBefore(
-        string $listenerId,
-        string $event,
-        string $listener,
-        int $priority = 0,
-        ?string $id = null
-    ): string;
-
-    public function addProvider(self $listenerProvider): void;
-
-    public function addSubscriber(SubscriberInterface $subscriber): void;
-
-    /**
-     * @param class-string<SubscriberInterface> $subscriber
+     * @param class-string<EventInterface<bool>> $event
+     * @param callable-string                    $listener
      *
      * @throws ContainerExceptionInterface
      */
-    public function addSubscriberService(string $subscriber): void;
+    public function bindListener(string $event, string $listener, int $priority = 0, ?string $id = null): string;
 
     /**
-     * @return Generator<callable(EventInterface):void> an iterable of callables type-compatible with $event
+     * @return Generator<Listener> an iterable of callables type-compatible with $event
      */
     public function getListenersForEvent(EventInterface $event): Generator;
 
     public function removeListener(string $listenerId): void;
-
-    /**
-     * @param class-string<ListenerProviderInterface> $providerId
-     */
-    public function removeProvider(string $providerId): void;
-
-    /**
-     * @param class-string<SubscriberInterface> $subscriberId
-     */
-    public function removeSubscriber(string $subscriberId): void;
 }
