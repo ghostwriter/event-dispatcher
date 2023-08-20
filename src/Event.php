f00b19ccb464dd067975fe83ce03e47d9c9a989e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-20 14:09:22 -0500

    Create Event.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Event.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\EventDispatcher;
+
+/**
+ * An Event that can stop propagation to any further Listeners.
+ *
+ * MUST be implemented to provide type-safety to both listeners and listener providers.
+ *
+ * @template TPropagationStopped of bool
+ */
+interface Event
+{
+    /**
+     * Determine if the previous listener halted propagation.
+     *
+     * @return TPropagationStopped is true ? true : false
+     */
+    public function isPropagationStopped(): bool;
+
+    /**
+     * Stop event propagation.
+     *
+     * @psalm-this-out self<true>
+     */
+    public function stopPropagation(): void;
+}
