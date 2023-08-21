d01872ec6c7a166b0250f817bc71f4b710d7312f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-21 06:16:20 -0500

    Update TestEventInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/TestEventInterface.php
+++ tests/Fixture/TestEventInterface.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Tests\Fixture;
 
-use Ghostwriter\EventDispatcher\Contract\EventInterface;
-
+use Ghostwriter\EventDispatcher\Event;
 
 /**
  * @template TPropagationStopped of bool
  *
- * @extends EventInterface<TPropagationStopped>
+ * @extends Event<TPropagationStopped>
  */
-interface TestEventInterface extends EventInterface
+interface TestEventInterface extends Event
 {
     public function write(string $event): void;
 
     /** @return array<array-key,string> */
     public function read(): array;
+
+    /**
+     * @return int<0,max>
+     */
+    public function count(): int;
 }
