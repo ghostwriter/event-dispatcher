0cf62a861e34faa60de034ebb40dab457c937ef7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-09 12:38:51 -0500

    Update Dispatcher.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -4,21 +4,91 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher;
 
-/**
- * Delegates dispatching an event to one or more dispatchers.
- */
-interface Dispatcher
-{
-    /**
-     * Provide all relevant listeners an event to process.
-     *
-     * @param Event<bool> $event
-     *
-     * @return Event<bool>
-     *
-     * @throws \Throwable
-     */
-    public function dispatch(Event $event): Event;
+use Fiber;
+use Ghostwriter\EventDispatcher\DispatcherInterface;
+use Ghostwriter\EventDispatcher\Event\ErrorEvent;
+use Ghostwriter\EventDispatcher\Event\ErrorInterface;
+use Ghostwriter\EventDispatcher\EventInterface;
+use Ghostwriter\EventDispatcher\ListenerProvider;
+use Ghostwriter\EventDispatcher\ListenerProviderInterface;
+use Throwable;
 
-    public function getListenerProvider(): ListenerProvider;
+final readonly class Dispatcher implements DispatcherInterface
+{
+    public function __construct(
+        private readonly ListenerProviderInterface $listenerProvider = new ListenerProvider()
+    ) {
+    }
+
+    /**
+     * @param EventInterface<bool> $event
+     *
+     * @return EventInterface<bool>
+     */
+    public function dispatch(EventInterface $event): EventInterface
+    {
+        // If event propagation has stopped, return the event.
+        if ($event->isPropagationStopped()) {
+            return $event;
+        }
+
+        $fiber = new Fiber(
+            /**
+             * @param EventInterface<bool> $event
+             *
+             * @return EventInterface<bool>
+             */
+            function (EventInterface $event): EventInterface {
+                $isErrorEvent = $event instanceof ErrorInterface;
+
+                foreach ($this->listenerProvider->getListenersForEvent($event) as $listener) {
+                    try {
+                        $listener($event);
+
+                        // If event propagation has stopped, break to return the event.
+                        if ($event->isPropagationStopped()) {
+                            /**
+                             * @var EventInterface<true> $event
+                             */
+                            return $event;
+                        }
+                        Fiber::suspend();
+                    } catch (Throwable $throwable) {
+                        // If an error is raised while processing an ErrorEvent,
+                        // re-throw the original throwable to prevent recursion.
+                        if ($isErrorEvent) {
+                            /**
+                             * @var ErrorEvent<bool> $event
+                             */
+                            throw $event->getThrowable();
+                        }
+
+                        // Dispatch a new ErrorEvent with the unhandled error raised.
+                        $this->dispatch(new ErrorEvent($event, $listener, $throwable));
+
+                        // Rethrow the original throwable; per PSR-14 specification.
+                        throw $throwable;
+                    }
+                }
+
+                /**
+                 * @var EventInterface<false> $event
+                 */
+                return $event;
+            }
+        );
+
+        $fiber->start($event);
+
+        while ($fiber->isSuspended()) {
+            $fiber->resume();
+        }
+
+        return $fiber->getReturn();
+    }
+
+    public function getListenerProvider(): ListenerProviderInterface
+    {
+        return $this->listenerProvider;
+    }
 }
