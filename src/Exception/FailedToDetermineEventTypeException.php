28ecf32537c159202e43e9fe7734775a040e3d78
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-11 14:50:33 -0500

    Update FailedToDetermineEventTypeException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDetermineEventTypeException.php
+++ src/Exception/FailedToDetermineEventTypeException.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Ghostwriter\EventDispatcher\Exception;
 
 use Ghostwriter\EventDispatcher\ExceptionInterface;
+use InvalidArgumentException;
 
-final class FailedToDetermineEventTypeException extends \InvalidArgumentException implements ExceptionInterface
+final class FailedToDetermineEventTypeException extends InvalidArgumentException implements ExceptionInterface
 {
 }
