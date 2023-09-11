c5584a1f9c458abf9213daec23cd132991d0dc7b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-11 14:50:37 -0500

    Update ListenerAlreadyExistsException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ListenerAlreadyExistsException.php
+++ src/Exception/ListenerAlreadyExistsException.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Ghostwriter\EventDispatcher\Exception;
 
 use Ghostwriter\EventDispatcher\ExceptionInterface;
+use InvalidArgumentException;
 
-final class ListenerAlreadyExistsException extends \InvalidArgumentException implements ExceptionInterface
+final class ListenerAlreadyExistsException extends InvalidArgumentException implements ExceptionInterface
 {
 }
