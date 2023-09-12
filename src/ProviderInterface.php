8c29d40c9fafd9de24415d7496b530b318d1cd7d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-12 16:51:11 -0500

    Update ProviderInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ProviderInterface.php
+++ src/ProviderInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher;
 
-use Generator;
 use Ghostwriter\EventDispatcher\Exception\EventNotFoundException;
 use Ghostwriter\EventDispatcher\Exception\FailedToDetermineEventTypeException;
 use Ghostwriter\EventDispatcher\Exception\ListenerAlreadyExistsException;
@@ -18,8 +17,16 @@ use Ghostwriter\EventDispatcher\Exception\SubscriberMustImplementSubscriberInter
 interface ProviderInterface
 {
     /**
-     * @param callable(EventInterface<bool>):void $listener
-     * @param null|class-string<EventInterface<bool>>  $event
+     * @param class-string<EventInterface<bool>> $event
+     * @param class-string|callable-string       $listener
+     *
+     * @throws ExceptionInterface
+     */
+    public function bind(string $event, string $listener, int $priority = 0, string $id = null): string;
+
+    /**
+     * @param callable(EventInterface<bool>):void     $listener
+     * @param class-string<EventInterface<bool>>|null $event
      *
      * @throws EventNotFoundException
      * @throws FailedToDetermineEventTypeException
@@ -31,19 +38,11 @@ interface ProviderInterface
     public function listen(callable $listener, int $priority = 0, string $event = null, string $id = null): string;
 
     /**
-     * @param class-string<EventInterface<bool>> $event
-     * @param class-string|callable-string       $listener
-     *
-     * @throws ExceptionInterface
-     */
-    public function bind(string $event, string $listener, int $priority = 0, string $id = null): string;
-
-    /**
      * @param EventInterface<bool> $event
      *
-     * @return Generator<ListenerInterface>
+     * @return \Generator<ListenerInterface>
      */
-    public function listeners(EventInterface $event): Generator;
+    public function listeners(EventInterface $event): \Generator;
 
     public function remove(string $listenerId): void;
 
