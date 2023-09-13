4edc01fb008e9d15112d20ffe14bd0d51ee1c9f5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-13 02:00:07 -0500

    Update Listener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener.php
+++ src/Listener.php
@@ -4,34 +4,16 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher;
 
-use Closure;
 use Ghostwriter\Container\Container;
-use Throwable;
 
 final readonly class Listener implements ListenerInterface
 {
     /**
-     * @param Closure(EventInterface<bool>):void $listener
+     * @param \Closure(EventInterface<bool>):void $listener
      */
     public function __construct(
-        private readonly Closure $listener
+        private readonly \Closure $listener
     ) {
-    }
-
-    /**
-     * @param class-string|callable-string $invokable
-     */
-    public static function fromInvokableClass(string $invokable): self
-    {
-        /**
-         * @var Closure(EventInterface<bool>):void $listener
-         * @throws Throwable
-         */
-        $listener = static function (EventInterface $event) use ($invokable): void {
-            Container::getInstance()->call($invokable, [$event]);
-        };
-
-        return new self($listener);
     }
 
     /**
@@ -40,5 +22,18 @@ final readonly class Listener implements ListenerInterface
     public function __invoke(EventInterface $event): void
     {
         ($this->listener)($event);
+    }
+
+    /**
+     * @param class-string|callable-string $invokable
+     */
+    public static function fromInvokableClass(string $invokable): self
+    {
+        /**
+         * @var \Closure(EventInterface<bool>):void $listener
+         */
+        $listener = static fn (EventInterface $event): mixed => Container::getInstance()->call($invokable, [$event]);
+
+        return new self($listener);
     }
 }
