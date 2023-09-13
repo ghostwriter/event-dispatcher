eeed97140d61bb66480202be7799c4d31ea9e448
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-13 02:05:35 -0500

    Update Provider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Provider.php
+++ src/Provider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher;
 
-use Generator;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\ContainerInterface;
 use Ghostwriter\Container\Exception\NotFoundExceptionInterface as ContainerNotFoundExceptionInterface;
@@ -19,20 +18,6 @@ use Ghostwriter\EventDispatcher\Exception\MissingEventParameterException;
 use Ghostwriter\EventDispatcher\Exception\MissingParameterTypeDeclarationException;
 use Ghostwriter\EventDispatcher\Exception\SubscriberAlreadyRegisteredException;
 use Ghostwriter\EventDispatcher\Exception\SubscriberMustImplementSubscriberInterfaceException;
-use ReflectionException;
-use ReflectionFunction;
-use ReflectionIntersectionType;
-use ReflectionNamedType;
-use ReflectionUnionType;
-use Throwable;
-use function array_key_exists;
-use function class_exists;
-use function interface_exists;
-use function is_a;
-use function krsort;
-use function method_exists;
-use function sprintf;
-use const SORT_NUMERIC;
 
 /**
  * Maps registered Listeners, Providers and Subscribers.
@@ -42,87 +27,11 @@ final class Provider implements ProviderInterface
     private readonly ContainerInterface $container;
 
     /**
-     * Map of registered Listeners, Providers and Subscribers.
-     *
-     * $listeners[$event][$priority][$listenerId] = $listener
+     * Map of registered Listeners.
      *
      * @var array<class-string<EventInterface<bool>>,array<int,array<string,ListenerInterface>>>
      */
     private array $listeners = [];
-
-    public function __construct(
-        ?ContainerInterface $container = null
-    ) {
-        $this->container = $container ?? Container::getInstance();
-    }
-
-    /**
-     * @throws EventNotFoundException
-     * @throws FailedToDetermineEventTypeException
-     * @throws ListenerAlreadyExistsException
-     * @throws MissingEventParameterException
-     * @throws MissingParameterTypeDeclarationException
-     */
-    public function listen(
-        callable $listener,
-        int $priority = 0,
-        string $event = null,
-        string $id = null
-    ): string {
-
-        /**
-         * @var string $id
-         * @var list<class-string<EventInterface<bool>>> $events
-         */
-        [$id, $events]= $this->resolve($listener, $event, $id);
-
-        foreach ($events as $event) {
-            if (!class_exists($event) && !interface_exists($event)) {
-                throw new EventNotFoundException($event);
-            }
-
-            if (
-                array_key_exists($event, $this->listeners) &&
-                array_key_exists($priority, $this->listeners[$event]) &&
-                array_key_exists($id, $this->listeners[$event][$priority])
-            ) {
-                throw new ListenerAlreadyExistsException($id);
-            }
-
-            /*
-             * @var class-string<EventInterface<bool>> $event
-             */
-            $this->listeners[$event][$priority][$id] = new Listener($listener(...));
-
-            krsort($this->listeners[$event], SORT_NUMERIC);
-        }
-
-        return $id;
-    }
-
-    /**
-     * @param class-string<SubscriberInterface> $subscriber
-     *
-     * @throws Throwable
-     * @throws SubscriberMustImplementSubscriberInterfaceException
-     * @throws ContainerNotFoundExceptionInterface
-     * @throws ContainerExceptionInterface
-     * @throws ExceptionInterface
-     */
-    public function subscribe(string $subscriber): void
-    {
-        if (!is_a($subscriber, SubscriberInterface::class, true)) {
-            throw new SubscriberMustImplementSubscriberInterfaceException($subscriber);
-        }
-
-        if (array_key_exists($subscriber, $this->subscribers)) {
-            throw new SubscriberAlreadyRegisteredException($subscriber);
-        }
-
-        $this->subscribers[$subscriber] = true;
-
-        $this->container->call($subscriber, [$this]);
-    }
 
     /**
      * Map of registered Subscribers.
@@ -131,6 +40,11 @@ final class Provider implements ProviderInterface
      */
     private array $subscribers = [];
 
+    public function __construct(
+        ContainerInterface $container = null
+    ) {
+        $this->container = $container ?? Container::getInstance();
+    }
 
     /**
      * @param class-string<EventInterface<bool>> $event
@@ -145,8 +59,12 @@ final class Provider implements ProviderInterface
      * @throws MissingEventParameterException
      * @throws MissingParameterTypeDeclarationException
      */
-    public function bind(string $event, string $listener, int $priority = 0, string $id = null): string
-    {
+    public function bind(
+        string $event,
+        string $listener,
+        int $priority = 0,
+        string $id = null
+    ): string {
         if (!is_a($event, EventInterface::class, true)) {
             throw new EventMustImplementEventInterfaceException();
         }
@@ -162,26 +80,67 @@ final class Provider implements ProviderInterface
         $id ??= $listener;
 
         if (
-            array_key_exists($event, $this->listeners) &&
-            array_key_exists($priority, $this->listeners[$event]) &&
-            array_key_exists($id, $this->listeners[$event][$priority])
+            \array_key_exists($event, $this->listeners)
+            && \array_key_exists($priority, $this->listeners[$event])
+            && \array_key_exists($id, $this->listeners[$event][$priority])
         ) {
             throw new ListenerAlreadyExistsException($id);
         }
 
         $this->listeners[$event][$priority][$id] = Listener::fromInvokableClass($listener);
 
-        krsort($this->listeners[$event], SORT_NUMERIC);
+        krsort($this->listeners[$event], \SORT_NUMERIC);
 
         return $id;
     }
 
     /**
+     * @param callable(EventInterface<bool>):void $listener
+     *
+     * @throws EventNotFoundException
+     * @throws FailedToDetermineEventTypeException
+     * @throws ListenerAlreadyExistsException
+     * @throws MissingEventParameterException
+     * @throws MissingParameterTypeDeclarationException
+     */
+    public function listen(
+        callable $listener,
+        int $priority = 0,
+        string $event = null,
+        string $id = null
+    ): string {
+        [$listenerId, $events] = $this->resolve($listener, $event, $id);
+
+        foreach ($events as $event) {
+            if (!class_exists($event) && !interface_exists($event)) {
+                throw new EventNotFoundException($event);
+            }
+
+            if (
+                \array_key_exists($event, $this->listeners)
+                && \array_key_exists($priority, $this->listeners[$event])
+                && \array_key_exists($listenerId, $this->listeners[$event][$priority])
+            ) {
+                throw new ListenerAlreadyExistsException($listenerId);
+            }
+
+            /*
+             * @var class-string<EventInterface<bool>> $event
+             */
+            $this->listeners[$event][$priority][$listenerId] = new Listener($listener(...));
+
+            krsort($this->listeners[$event], \SORT_NUMERIC);
+        }
+
+        return $listenerId;
+    }
+
+    /**
      * @param EventInterface<bool> $event
      *
-     * @return Generator<ListenerInterface>
+     * @return \Generator<int,ListenerInterface,mixed,void>
      */
-    public function listeners(EventInterface $event): Generator
+    public function listeners(EventInterface $event): \Generator
     {
         foreach ($this->listeners as $type => $priorities) {
             if (!$event instanceof $type) {
@@ -207,7 +166,7 @@ final class Provider implements ProviderInterface
     {
         foreach ($this->listeners as $event => $listeners) {
             foreach ($listeners as $priority => $listener) {
-                if (array_key_exists($listenerId, $listener)) {
+                if (\array_key_exists($listenerId, $listener)) {
                     unset($this->listeners[$event][$priority][$listenerId]);
 
                     return;
@@ -219,26 +178,86 @@ final class Provider implements ProviderInterface
     }
 
     /**
+     * @param class-string<SubscriberInterface> $subscriber
+     *
+     * @throws \Throwable
+     * @throws SubscriberMustImplementSubscriberInterfaceException
+     * @throws ContainerNotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     * @throws ExceptionInterface
+     */
+    public function subscribe(string $subscriber): void
+    {
+        if (!is_a($subscriber, SubscriberInterface::class, true)) {
+            throw new SubscriberMustImplementSubscriberInterfaceException($subscriber);
+        }
+
+        if (\array_key_exists($subscriber, $this->subscribers)) {
+            throw new SubscriberAlreadyRegisteredException($subscriber);
+        }
+
+        $this->subscribers[$subscriber] = true;
+
+        $this->container->call($subscriber, [$this]);
+    }
+
+    /** @return array<class-string<EventInterface<bool>>> */
+    private function eventFromReflectionIntersectionOrUnionType(
+        \ReflectionIntersectionType|\ReflectionUnionType $reflectionIntersectionOrUnionType
+    ): array {
+        $events = [];
+
+        foreach ($reflectionIntersectionOrUnionType->getTypes() as $reflectionType) {
+            if ($reflectionType instanceof \ReflectionNamedType) {
+                /** @var class-string<EventInterface<bool>> $reflectionTypeName */
+                $reflectionTypeName = $reflectionType->getName();
+
+                $events[] = $reflectionTypeName;
+
+                continue;
+            }
+
+            $events = array_merge($events, $this->eventFromReflectionIntersectionOrUnionType($reflectionType));
+        }
+
+        /** @var array<class-string<EventInterface<bool>>> */
+        return $events;
+    }
+
+    /**
      * @param callable(EventInterface<bool>):void $listener
+     *
+     * @return list{non-falsy-string,list{class-string<EventInterface<bool>>}} [$listenerId, $events]
      *
      * @throws FailedToDetermineEventTypeException
      * @throws MissingEventParameterException
      * @throws MissingParameterTypeDeclarationException
      */
-    private function resolve(callable $listener, ?string $event = null, ?string $id = null): array
-    {
+    private function resolve(
+        callable $listener,
+        string $event = null,
+        string $id = null
+    ): array {
+        /**
+         * Closure(EventInterface<bool>):void.
+         */
         $closure = $listener(...);
 
         try {
-            $reflectionFunction = new ReflectionFunction($closure);
-        } catch (ReflectionException $reflectionException) {
+            $reflectionFunction = new \ReflectionFunction($closure);
+        } catch (\ReflectionException $reflectionException) {
             throw new FailedToDetermineEventTypeException($reflectionException->getMessage());
         }
 
         $id ??= sprintf('%s:%s', $reflectionFunction->getFileName(), $reflectionFunction->getStartLine());
 
+        $listenerId = 'listener.' . md5($id);
+
         if (null !== $event) {
-            return [$id, [$event]];
+            /**
+             * @var list{non-falsy-string,list{class-string<EventInterface<bool>>}}
+             */
+            return [$listenerId, [$event]];
         }
 
         $parameters = $reflectionFunction->getParameters();
@@ -252,24 +271,28 @@ final class Provider implements ProviderInterface
         foreach ($parameters as $parameter) {
             $reflectionType = $parameter->getType();
 
-            if ($reflectionType instanceof ReflectionNamedType) {
+            if ($reflectionType instanceof \ReflectionNamedType) {
                 $events[] = $reflectionType->getName();
 
                 continue;
             }
 
             if (
-                !$reflectionType instanceof ReflectionUnionType &&
-                !$reflectionType instanceof ReflectionIntersectionType
+                !$reflectionType instanceof \ReflectionUnionType
+                && !$reflectionType instanceof \ReflectionIntersectionType
             ) {
                 throw new MissingParameterTypeDeclarationException($parameter->getName());
             }
 
-            foreach ($reflectionType->getTypes() as $reflectionNamedType) {
-                $events[] = $reflectionNamedType->getName();
-            }
+            $events = array_merge(
+                $events,
+                $this->eventFromReflectionIntersectionOrUnionType($reflectionType)
+            );
         }
 
-        return [$id, $events];
+        /**
+         * @var list{non-falsy-string,list{class-string<EventInterface<bool>>}}
+         */
+        return [$listenerId, $events];
     }
 }
