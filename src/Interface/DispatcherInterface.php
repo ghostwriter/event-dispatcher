b5e925e43b38ad995db84a6660d3941fb81f8396
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-13 13:10:32 -0500

    Create DispatcherInterface.php

---
+++ src/Interface/DispatcherInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\EventDispatcher\Interface;
+
+use Throwable;
+
+/**
+ * Delegates dispatching an event to one or more dispatchers.
+ */
+interface DispatcherInterface
+{
+    /**
+     * Provide all relevant listeners an event to process.
+     *
+     * @param EventInterface<bool> $event
+     *
+     * @return EventInterface<bool>
+     *
+     * @throws Throwable
+     */
+    public function dispatch(EventInterface $event): EventInterface;
+}
