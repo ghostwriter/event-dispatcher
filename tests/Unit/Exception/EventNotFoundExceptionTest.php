3e9149e7b6a2777676e8e1d73f7e9ec72846a54e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-13 17:30:15 -0500

    Create EventNotFoundExceptionTest.php

---
+++ tests/Unit/Exception/EventNotFoundExceptionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\EventDispatcher\Tests\Unit\Exception;
+
+use Ghostwriter\EventDispatcher\Exception\EventNotFoundException;
+use Ghostwriter\EventDispatcher\ListenerProvider;
+use Ghostwriter\EventDispatcher\Tests\Fixture\TestEventListener;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(EventNotFoundException::class)]
+#[CoversClass(ListenerProvider::class)]
+final class EventNotFoundExceptionTest extends TestCase
+{
+    public function testThrowsEventNotFoundException(): void {
+        $provider = new ListenerProvider();
+
+        $this->expectException(EventNotFoundException::class);
+
+        $provider->bind('does-not-exist',TestEventListener::class);
+    }
+}
