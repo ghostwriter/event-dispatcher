3e2b67b672ec0a829ac3efd40c51b7f4d8e9971e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-13 17:26:43 -0500

    Update EventInterface.php

--- src/Interface/EventInterface.php
+++ src/Interface/EventInterface.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace Ghostwriter\EventDispatcher\Interface;
 
 /**
- * @template TDone of bool
+ * @template TStopPropagation of bool
  */
 interface EventInterface
 {
     /**
-     * @return (TDone is true ? true : false)
+     * @return (TStopPropagation is true ? true : false)
      */
-    public function isDone(): bool;
+    public function isPropagationStopped(): bool;
 
     /**
      * @psalm-this-out self<true>
      */
-    public function done(): void;
+    public function stopPropagation(): void;
 }
