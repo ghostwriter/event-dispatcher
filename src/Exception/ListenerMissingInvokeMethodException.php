b5aa1c703933d028ec325a3998061ae70cb4c894
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-13 17:29:30 -0500

    Update ListenerMissingInvokeMethodException.php

--- src/Exception/ListenerMissingInvokeMethodException.php
+++ src/Exception/ListenerMissingInvokeMethodException.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Exception;
 
-use Ghostwriter\EventDispatcher\ExceptionInterface;
+use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
+use RuntimeException;
 
-final class ListenerMissingInvokeMethodException extends \RuntimeException implements ExceptionInterface
+final class ListenerMissingInvokeMethodException extends RuntimeException implements ExceptionInterface
 {
 }
