821992c5194c5cdfa67027a6286e43b050599fd9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-13 17:29:40 -0500

    Update SubscriberAlreadyRegisteredException.php

--- src/Exception/SubscriberAlreadyRegisteredException.php
+++ src/Exception/SubscriberAlreadyRegisteredException.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Exception;
 
-use Ghostwriter\EventDispatcher\ExceptionInterface;
+use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
+use InvalidArgumentException;
 
-final class SubscriberAlreadyRegisteredException extends \InvalidArgumentException implements ExceptionInterface
+final class SubscriberAlreadyRegisteredException extends InvalidArgumentException implements ExceptionInterface
 {
 }
