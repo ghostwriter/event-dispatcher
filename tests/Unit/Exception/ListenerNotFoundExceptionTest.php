b207432e9833cba6bcd6f10a6849c4e72e96df5b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:22:20 -0600

    Update ListenerNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/ListenerNotFoundExceptionTest.php
+++ tests/Unit/Exception/ListenerNotFoundExceptionTest.php
@@ -2,38 +2,50 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\EventDispatcher\Tests\Unit\Exception;
+namespace Ghostwriter\EventDispatcherTests\Unit\Exception;
 
+use Ghostwriter\EventDispatcher\AbstractEvent;
+use Ghostwriter\EventDispatcher\Event\ErrorEvent;
+use Ghostwriter\EventDispatcher\EventDispatcher;
+use Ghostwriter\EventDispatcher\EventServiceProvider;
 use Ghostwriter\EventDispatcher\Exception\ListenerNotFoundException;
 use Ghostwriter\EventDispatcher\ListenerProvider;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEvent;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEventListener;
+use Ghostwriter\EventDispatcher\Trait\EventTrait;
+use Ghostwriter\EventDispatcherTests\Fixture\TestEvent;
+use Ghostwriter\EventDispatcherTests\Fixture\TestEventListener;
+use Ghostwriter\EventDispatcherTests\Unit\AbstractTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\UsesClass;
-use PHPUnit\Framework\TestCase;
+use Throwable;
 
-#[CoversClass(ListenerNotFoundException::class)]
+#[CoversClass(AbstractEvent::class)]
+#[CoversClass(EventDispatcher::class)]
+#[CoversClass(ErrorEvent::class)]
+#[CoversClass(EventServiceProvider::class)]
+#[CoversClass(EventTrait::class)]
 #[CoversClass(ListenerProvider::class)]
-final class ListenerNotFoundExceptionTest extends TestCase
+#[CoversClass(ListenerNotFoundException::class)]
+final class ListenerNotFoundExceptionTest extends AbstractTestCase
 {
-    public function testRemoveThrowsListenerNotFoundException(): void
+    /**
+     * @psalm-suppress UndefinedClass
+     *
+     * @throws Throwable
+     */
+    public function testBind(): void
     {
-
-        $provider = new ListenerProvider();
-
-        $this->expectException(ListenerNotFoundException::class);
-
-        $provider->remove(TestEventListener::class);
-    }
-
-    public function testBindThrowsListenerNotFoundException(): void
-    {
-
         $provider = new ListenerProvider();
 
         $this->expectException(ListenerNotFoundException::class);
 
         $provider->bind(TestEvent::class, 'NonExistTestEventListener');
     }
-}
 
+    public function testRemove(): void
+    {
+        $provider = new ListenerProvider();
+
+        $this->expectException(ListenerNotFoundException::class);
+
+        $provider->remove(TestEventListener::class);
+    }
+}
