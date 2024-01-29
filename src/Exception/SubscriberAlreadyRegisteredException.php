05cd05862db1d806ce9728d357a76683a4613304
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:27:33 -0600

    Update SubscriberAlreadyRegisteredException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/SubscriberAlreadyRegisteredException.php
+++ src/Exception/SubscriberAlreadyRegisteredException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Exception;
 
-use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
+use Ghostwriter\EventDispatcher\Interface\EventDispatcherExceptionInterface;
 use InvalidArgumentException;
 
-final class SubscriberAlreadyRegisteredException extends InvalidArgumentException implements ExceptionInterface
+final class SubscriberAlreadyRegisteredException extends InvalidArgumentException implements EventDispatcherExceptionInterface
 {
 }
