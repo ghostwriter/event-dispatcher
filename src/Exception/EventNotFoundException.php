763825d74779a4a3b6d87484e2c05a7dd10e8a12
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:27:45 -0600

    Update EventNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/EventNotFoundException.php
+++ src/Exception/EventNotFoundException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Exception;
 
-use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
+use Ghostwriter\EventDispatcher\Interface\EventDispatcherExceptionInterface;
 use InvalidArgumentException;
 
-final class EventNotFoundException extends InvalidArgumentException implements ExceptionInterface
+final class EventNotFoundException extends InvalidArgumentException implements EventDispatcherExceptionInterface
 {
 }
