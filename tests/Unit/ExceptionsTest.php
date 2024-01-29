d65004af8f8f32370f5e32e6a57296ae30f401ac
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:22:08 -0600

    Update ExceptionsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ExceptionsTest.php
+++ tests/Unit/ExceptionsTest.php
@@ -2,8 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\EventDispatcher\Tests\Unit;
+namespace Ghostwriter\EventDispatcherTests\Unit;
 
+use Ghostwriter\EventDispatcher\AbstractEvent;
+use Ghostwriter\EventDispatcher\Event\ErrorEvent;
+use Ghostwriter\EventDispatcher\EventDispatcher;
+use Ghostwriter\EventDispatcher\EventServiceProvider;
 use Ghostwriter\EventDispatcher\Exception\EventMustImplementEventInterfaceException;
 use Ghostwriter\EventDispatcher\Exception\EventNotFoundException;
 use Ghostwriter\EventDispatcher\Exception\FailedToDetermineEventTypeException;
@@ -14,11 +18,21 @@ use Ghostwriter\EventDispatcher\Exception\MissingEventParameterException;
 use Ghostwriter\EventDispatcher\Exception\MissingParameterTypeDeclarationException;
 use Ghostwriter\EventDispatcher\Exception\SubscriberAlreadyRegisteredException;
 use Ghostwriter\EventDispatcher\Exception\SubscriberMustImplementSubscriberInterfaceException;
-use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
+use Ghostwriter\EventDispatcher\Interface\EventDispatcherExceptionInterface;
+use Ghostwriter\EventDispatcher\ListenerProvider;
+use Ghostwriter\EventDispatcher\Trait\EventTrait;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
 use Throwable;
 
+use function is_a;
+use function sprintf;
+
+#[CoversClass(AbstractEvent::class)]
+#[CoversClass(EventDispatcher::class)]
+#[CoversClass(ErrorEvent::class)]
+#[CoversClass(EventServiceProvider::class)]
+#[CoversClass(EventTrait::class)]
+#[CoversClass(ListenerProvider::class)]
 #[CoversClass(EventMustImplementEventInterfaceException::class)]
 #[CoversClass(EventNotFoundException::class)]
 #[CoversClass(FailedToDetermineEventTypeException::class)]
@@ -29,7 +43,7 @@ use Throwable;
 #[CoversClass(MissingParameterTypeDeclarationException::class)]
 #[CoversClass(SubscriberAlreadyRegisteredException::class)]
 #[CoversClass(SubscriberMustImplementSubscriberInterfaceException::class)]
-final class ExceptionsTest extends TestCase
+final class ExceptionsTest extends AbstractTestCase
 {
     /**
      * @var array<class-string<Throwable>>
@@ -51,8 +65,8 @@ final class ExceptionsTest extends TestCase
     {
         foreach (self::EXCEPTIONS as $exception) {
             self::assertTrue(
-                is_a($exception, ExceptionInterface::class, true),
-                sprintf('Exception "%s" does not implement "%s"', $exception, ExceptionInterface::class)
+                is_a($exception, EventDispatcherExceptionInterface::class, true),
+                sprintf('Exception "%s" does not implement "%s"', $exception, EventDispatcherExceptionInterface::class)
             );
         }
     }
