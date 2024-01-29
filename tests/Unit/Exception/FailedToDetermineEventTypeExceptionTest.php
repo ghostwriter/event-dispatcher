732fd8d592a00aca447df4473a5fd3b2abe302fa
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:22:27 -0600

    Update FailedToDetermineEventTypeExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDetermineEventTypeExceptionTest.php
+++ tests/Unit/Exception/FailedToDetermineEventTypeExceptionTest.php
@@ -2,18 +2,29 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\EventDispatcher\Tests\Unit\Exception;
+namespace Ghostwriter\EventDispatcherTests\Unit\Exception;
 
+use Ghostwriter\EventDispatcher\AbstractEvent;
+use Ghostwriter\EventDispatcher\Event\ErrorEvent;
+use Ghostwriter\EventDispatcher\EventDispatcher;
+use Ghostwriter\EventDispatcher\EventServiceProvider;
 use Ghostwriter\EventDispatcher\Exception\FailedToDetermineEventTypeException;
+use Ghostwriter\EventDispatcher\ListenerProvider;
+use Ghostwriter\EventDispatcher\Trait\EventTrait;
+use Ghostwriter\EventDispatcherTests\Unit\AbstractTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
 
+#[CoversClass(AbstractEvent::class)]
+#[CoversClass(EventDispatcher::class)]
+#[CoversClass(ErrorEvent::class)]
+#[CoversClass(EventServiceProvider::class)]
+#[CoversClass(EventTrait::class)]
+#[CoversClass(ListenerProvider::class)]
 #[CoversClass(FailedToDetermineEventTypeException::class)]
-final class FailedToDetermineEventTypeExceptionTest extends TestCase
+final class FailedToDetermineEventTypeExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
-        $this->assertTrue(true);
+        self::assertTrue(true);
     }
 }
-
