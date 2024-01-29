c62c721aed2983d2388784be96f84d88e0d9f90d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:27:58 -0600

    Create EventServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/EventServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\EventDispatcher;
+
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ServiceProviderInterface;
+use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
+use Ghostwriter\EventDispatcher\Interface\ListenerProviderInterface;
+use Throwable;
+
+final readonly class EventServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @throws Throwable
+     */
+    public function __invoke(ContainerInterface $container): void
+    {
+        $container->alias(EventDispatcherInterface::class, EventDispatcher::class);
+        $container->alias(ListenerProviderInterface::class, ListenerProvider::class);
+    }
+}
