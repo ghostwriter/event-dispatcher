3dcddef9afb17c11c0534ddb077bf181a3dded90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:37:04 -0600

    Update TestEventSubscriber.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Subscriber/TestEventSubscriber.php
+++ tests/Fixture/Subscriber/TestEventSubscriber.php
@@ -2,14 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\EventDispatcher\Tests\Fixture\Subscriber;
+namespace Ghostwriter\EventDispatcherTests\Fixture\Subscriber;
 
-use Ghostwriter\EventDispatcher\Interface\EventInterface;
 use Ghostwriter\EventDispatcher\Interface\ListenerProviderInterface;
 use Ghostwriter\EventDispatcher\Interface\SubscriberInterface;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEvent;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEventInterface;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEventListener;
+use Ghostwriter\EventDispatcherTests\Fixture\TestEvent;
+use Ghostwriter\EventDispatcherTests\Fixture\TestEventListener;
 use Throwable;
 
 final readonly class TestEventSubscriber implements SubscriberInterface
@@ -20,15 +18,15 @@ final readonly class TestEventSubscriber implements SubscriberInterface
     public function __invoke(ListenerProviderInterface $provider): void
     {
         // Invokable class '::__invoke'
-        //        $provider->bind(TestEvent::class, TestEventListener::class);
+//                $provider->bind(TestEvent::class, TestEventListener::class);
         $provider->listen(TestEventListener::class);
 
         // Invokable function
         //        $provider->bind(TestEvent::class, 'Ghostwriter\EventDispatcher\Tests\Fixture\listenerFunction');
-        $provider->listen('Ghostwriter\EventDispatcher\Tests\Fixture\listenerFunction');
+//        $provider->listen('Ghostwriter\EventDispatcher\Tests\Fixture\listenerFunction');
 
         // Invokable static method
         //        $provider->bind(TestEvent::class, TestEventListener::class . '::onStatic');
-        $provider->listen(TestEventListener::class . '::onStatic');
+//        $provider->listen(TestEventListener::class . '::onStatic');
     }
 }
