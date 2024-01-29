2a49a10f9fa482edf784f18474fd7911b47d2771
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 03:22:56 -0600

    Update ErrorEventTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ErrorEventTest.php
+++ tests/Unit/ErrorEventTest.php
@@ -2,66 +2,29 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\EventDispatcher\Tests\Unit;
+namespace Ghostwriter\EventDispatcherTests\Unit;
 
-use Closure;
-use Ghostwriter\EventDispatcher\Dispatcher;
+use Ghostwriter\EventDispatcher\AbstractEvent;
 use Ghostwriter\EventDispatcher\Event\ErrorEvent;
-use Ghostwriter\EventDispatcher\Interface\DispatcherInterface;
+use Ghostwriter\EventDispatcher\EventDispatcher;
+use Ghostwriter\EventDispatcher\EventServiceProvider;
 use Ghostwriter\EventDispatcher\Interface\Event\ErrorEventInterface;
 use Ghostwriter\EventDispatcher\Interface\EventInterface;
-use Ghostwriter\EventDispatcher\Interface\ListenerProviderInterface;
 use Ghostwriter\EventDispatcher\ListenerProvider;
-use Ghostwriter\EventDispatcher\Tests\Fixture\Listener\ErrorEventListener;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEvent;
-use Ghostwriter\EventDispatcher\Tests\Fixture\TestEventListener;
 use Ghostwriter\EventDispatcher\Trait\EventTrait;
+use Ghostwriter\EventDispatcherTests\Fixture\Listener\ErrorEventListener;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 use RuntimeException;
 use Throwable;
 
-#[CoversClass(EventTrait::class)]
-#[CoversClass(Dispatcher::class)]
+#[CoversClass(AbstractEvent::class)]
+#[CoversClass(EventDispatcher::class)]
 #[CoversClass(ErrorEvent::class)]
+#[CoversClass(EventServiceProvider::class)]
+#[CoversClass(EventTrait::class)]
 #[CoversClass(ListenerProvider::class)]
-final class ErrorEventTest extends PHPUnitTestCase
+final class ErrorEventTest extends AbstractTestCase
 {
-    /**
-     * @var int
-     */
-    public const ERROR_CODE = 42;
-
-    /**
-     * @var string
-     */
-    public const ERROR_MESSAGE = 'Could not handle the event!';
-
-    private DispatcherInterface $dispatcher;
-
-    private ErrorEventInterface $errorEvent;
-
-    /**
-     * @var callable(EventInterface<bool>):void $listener
-     */
-    private mixed $listener;
-
-    private ListenerProviderInterface $provider;
-
-    private TestEvent $testEvent;
-
-    private Throwable $throwable;
-
-    protected function setUp(): void
-    {
-        $this->testEvent = new TestEvent();
-        $this->throwable = new RuntimeException(self::ERROR_MESSAGE, self::ERROR_CODE);
-        $this->provider = new ListenerProvider();
-        $this->dispatcher = new Dispatcher();
-        $this->listener = ([new TestEventListener(), '__invoke'])(...);
-        $this->errorEvent = new ErrorEvent($this->testEvent, $this->listener, $this->throwable);
-    }
-
     /**
      * @throws Throwable
      */
@@ -74,15 +37,15 @@ final class ErrorEventTest extends PHPUnitTestCase
         self::assertSame(self::ERROR_MESSAGE, $this->errorEvent->getThrowable()->getMessage());
         self::assertSame(self::ERROR_CODE, $this->errorEvent->getThrowable()->getCode());
 
-        $this->provider->listen(ErrorEventListener::class);
+        $this->listenerProvider->listen(ErrorEventListener::class);
 
-        $this->dispatcher = new Dispatcher($this->provider);
+        $this->eventDispatcher = new EventDispatcher($this->listenerProvider);
 
         $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage(self::ERROR_MESSAGE);
         $this->expectExceptionCode(self::ERROR_CODE);
 
-        $this->dispatcher->dispatch($this->errorEvent);
+        $this->eventDispatcher->dispatch($this->errorEvent);
     }
 
     public function testErrorEventImplementsErrorEventInterface(): void
