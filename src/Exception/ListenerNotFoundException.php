d8e462831e4e29184e000d140014de2d222201e3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-06 13:54:38 -0600

    Update ListenerNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ListenerNotFoundException.php
+++ src/Exception/ListenerNotFoundException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Exception;
 
-use Ghostwriter\EventDispatcher\Interface\EventDispatcherExceptionInterface;
+use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ListenerNotFoundException extends InvalidArgumentException implements EventDispatcherExceptionInterface
+final class ListenerNotFoundException extends InvalidArgumentException implements ExceptionInterface
 {
 }
