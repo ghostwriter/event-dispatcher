0315b01fdb9762f6bdf8fc95ae12cb0b8ae2f884
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 14:35:18 -0500

    Update UnionParameterTypeDeclarationListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Listener/UnionParameterTypeDeclarationListener.php
+++ tests/Fixture/Listener/UnionParameterTypeDeclarationListener.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\EventDispatcherTests\Fixture\Listener;
+namespace Tests\Fixture\Listener;
 
-use Ghostwriter\EventDispatcherTests\Fixture\TestEvent;
-use Ghostwriter\EventDispatcherTests\Fixture\TestEvent2;
+use Tests\Fixture\TestEvent;
+use Tests\Fixture\TestEvent2;
 
 final class UnionParameterTypeDeclarationListener
 {
