2bf71ce2c106fdd630e6453ee86edd7b343336f6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-10 21:31:06 -0500

    Update EventMustImplementEventInterfaceException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/EventMustImplementEventInterfaceException.php
+++ src/Exception/EventMustImplementEventInterfaceException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\EventDispatcher\Exception;
 use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class EventMustImplementEventInterfaceException extends InvalidArgumentException implements ExceptionInterface
-{
-}
+final class EventMustImplementEventInterfaceException extends InvalidArgumentException implements ExceptionInterface {}
