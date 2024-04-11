6b4c4ebb1aeddbc750e656a944eb19e24234251e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-10 21:31:08 -0500

    Add SubscriberNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/SubscriberNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\EventDispatcher\Exception;
+
+use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
+use RuntimeException;
+
+final class SubscriberNotFoundException extends RuntimeException implements ExceptionInterface {}
