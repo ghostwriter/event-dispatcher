67c0140028207fb2c0b01412cbc5da3809955b2f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 21:57:22 -0500

    Update TestEvent2.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/TestEvent2.php
+++ tests/Fixture/TestEvent2.php
@@ -4,16 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Fixture;
 
-use Ghostwriter\EventDispatcher\Trait\EventTrait;
-use Tests\Fixture\TestEventInterface;
 use Ghostwriter\EventDispatcher\Interface\EventInterface;
 
-/**
- * @template TStopPropagation of bool
- * @implements EventInterface<TStopPropagation>
- */
 final class TestEvent2 implements EventInterface
 {
-    /** @use EventTrait<TStopPropagation> */
-    use EventTrait;
 }
