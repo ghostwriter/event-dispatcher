95bd6a53d1727f0fddf81f1a9e2e31172ece6c49
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 21:57:23 -0500

    Update EventInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/EventInterface.php
+++ src/Interface/EventInterface.php
@@ -4,18 +4,4 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Interface;
 
-/**
- * @template TStopPropagation of bool
- */
-interface EventInterface
-{
-    /**
-     * @return (TStopPropagation is true ? true : false)
-     */
-    public function isPropagationStopped(): bool;
-
-    /**
-     * @psalm-this-out self<true>
-     */
-    public function stopPropagation(): void;
-}
+interface EventInterface {}
