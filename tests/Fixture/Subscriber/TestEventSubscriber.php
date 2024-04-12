cea39464b3b1f460d93c9e661c11f890463ac539
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 21:57:22 -0500

    Update TestEventSubscriber.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Subscriber/TestEventSubscriber.php
+++ tests/Fixture/Subscriber/TestEventSubscriber.php
@@ -9,24 +9,20 @@ use Ghostwriter\EventDispatcher\Interface\SubscriberInterface;
 use Tests\Fixture\TestEvent;
 use Tests\Fixture\TestEventListener;
 use Throwable;
+use function Tests\Fixture\listenerFunction;
+use Tests\Fixture\Listener\LogTestEventExceptionMessageListener;
+use Ghostwriter\EventDispatcher\Interface\Event\ErrorEventInterface;
 
 final readonly class TestEventSubscriber implements SubscriberInterface
 {
     /**
      * @throws Throwable
      */
-    public function __invoke(ListenerProviderInterface $provider): void
+    #[\Override]
+    public function __invoke(ListenerProviderInterface $listenerProvider): void
     {
         // Invokable class '::__invoke'
-//                $provider->bind(TestEvent::class, TestEventListener::class);
-        $provider->listen(TestEvent::class,TestEventListener::class);
-
-        // Invokable function
-        //        $provider->bind(TestEvent::class, 'Ghostwriter\EventDispatcher\Tests\Fixture\listenerFunction');
-//        $provider->listen('Ghostwriter\EventDispatcher\Tests\Fixture\listenerFunction');
-
-        // Invokable static method
-        //        $provider->bind(TestEvent::class, TestEventListener::class . '::onStatic');
-//        $provider->listen(TestEventListener::class . '::onStatic');
+        $listenerProvider->listen(TestEvent::class, TestEventListener::class);
+        $listenerProvider->listen(ErrorEventInterface::class, LogTestEventExceptionMessageListener::class);
     }
 }
