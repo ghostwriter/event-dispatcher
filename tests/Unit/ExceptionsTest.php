ab5100af8cf4b8b9474c3b587a34bd72a08a1a84
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 21:57:19 -0500

    Update ExceptionsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ExceptionsTest.php
+++ tests/Unit/ExceptionsTest.php
@@ -7,7 +7,6 @@ namespace Tests\Unit;
 use Ghostwriter\EventDispatcher\Event\ErrorEvent;
 use Ghostwriter\EventDispatcher\EventDispatcher;
 use Ghostwriter\EventDispatcher\EventServiceProvider;
-use Ghostwriter\EventDispatcher\Exception\EventMustImplementEventInterfaceException;
 use Ghostwriter\EventDispatcher\Exception\EventNotFoundException;
 use Ghostwriter\EventDispatcher\Exception\FailedToDetermineEventTypeException;
 use Ghostwriter\EventDispatcher\Exception\ListenerAlreadyExistsException;
@@ -17,7 +16,6 @@ use Ghostwriter\EventDispatcher\Exception\SubscriberAlreadyRegisteredException;
 use Ghostwriter\EventDispatcher\Exception\SubscriberMustImplementSubscriberInterfaceException;
 use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
 use Ghostwriter\EventDispatcher\ListenerProvider;
-use Ghostwriter\EventDispatcher\Trait\EventTrait;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Throwable;
 
@@ -27,9 +25,7 @@ use function sprintf;
 #[CoversClass(EventDispatcher::class)]
 #[CoversClass(ErrorEvent::class)]
 #[CoversClass(EventServiceProvider::class)]
-#[CoversClass(EventTrait::class)]
 #[CoversClass(ListenerProvider::class)]
-#[CoversClass(EventMustImplementEventInterfaceException::class)]
 #[CoversClass(EventNotFoundException::class)]
 #[CoversClass(FailedToDetermineEventTypeException::class)]
 #[CoversClass(ListenerAlreadyExistsException::class)]
@@ -43,7 +39,6 @@ final class ExceptionsTest extends AbstractTestCase
      * @var array<class-string<Throwable>>
      */
     public const EXCEPTIONS = [
-        EventMustImplementEventInterfaceException::class,
         EventNotFoundException::class,
         FailedToDetermineEventTypeException::class,
         ListenerAlreadyExistsException::class,
