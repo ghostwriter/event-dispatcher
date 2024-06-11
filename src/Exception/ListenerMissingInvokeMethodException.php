50d64c1413cf37903ea4d78caa946f30ff00864c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:41:56 -0500

    Update ListenerMissingInvokeMethodException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ListenerMissingInvokeMethodException.php
+++ src/Exception/ListenerMissingInvokeMethodException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\EventDispatcher\Exception;
 use Ghostwriter\EventDispatcher\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class ListenerMissingInvokeMethodException extends RuntimeException implements ExceptionInterface {}
+final class ListenerMissingInvokeMethodException extends RuntimeException implements ExceptionInterface
+{
+}
