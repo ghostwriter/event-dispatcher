a4223930b6e50983c76641ea3fadd59699d55a12
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-04-23 14:18:55 -0500

    Use ALIAS/FACTORY constants in provider
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Container/EventDispatcherProvider.php
+++ src/Container/EventDispatcherProvider.php
@@ -4,30 +4,38 @@ declare(strict_types=1);
 
 namespace Ghostwriter\EventDispatcher\Container;
 
-use Ghostwriter\Container\Interface\BuilderInterface;
+use Ghostwriter\Container\Interface\Service\FactoryInterface;
 use Ghostwriter\Container\Service\Provider\AbstractProvider;
 use Ghostwriter\EventDispatcher\EventDispatcher;
 use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
 use Ghostwriter\EventDispatcher\Interface\ListenerProviderInterface;
 use Ghostwriter\EventDispatcher\ListenerProvider;
-use Override;
 use Psr\EventDispatcher\EventDispatcherInterface as PsrEventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface as PsrListenerProviderInterface;
-use Throwable;
 
 /**
  * @see EventDispatcherTest
  */
 final class EventDispatcherProvider extends AbstractProvider
 {
-    /** @throws Throwable */
-    #[Override]
-    public function register(BuilderInterface $builder): void
-    {
-        $builder->alias(EventDispatcherInterface::class, EventDispatcher::class);
-        $builder->alias(ListenerProviderInterface::class, ListenerProvider::class);
-        $builder->alias(PsrEventDispatcherInterface::class, EventDispatcherInterface::class);
-        $builder->alias(PsrListenerProviderInterface::class, ListenerProviderInterface::class);
-        $builder->factory(ListenerProvider::class, ListenerProviderFactory::class);
-    }
+    /**
+     * alias => service.
+     *
+     * @var array<class-string,class-string>
+     */
+    public const array ALIAS = [
+        EventDispatcherInterface::class => EventDispatcher::class,
+        ListenerProviderInterface::class => ListenerProvider::class,
+        PsrEventDispatcherInterface::class => EventDispatcherInterface::class,
+        PsrListenerProviderInterface::class => ListenerProviderInterface::class,
+    ];
+
+    /**
+     * service => factory.
+     *
+     * @var array<class-string,class-string<FactoryInterface>>
+     */
+    public const array FACTORY = [
+        ListenerProvider::class => ListenerProviderFactory::class,
+    ];
 }
